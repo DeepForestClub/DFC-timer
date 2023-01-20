28069bf76db8c5699f9cbad5bf7ff712dd6bcaf0
lest-day <106459514+lest-day@users.noreply.github.com>
2023-01-20 11:34:08 +0800

    Update timer-selector.js
    Signed-off-by: lest-day <106459514+lest-day@users.noreply.github.com>

--- timer-selector.js
+++ timer-selector.js
@@ -217,7 +217,7 @@ function buildUrl(language, startDate, durationMs, progressMessage, finishedMess
 
 function buildWikitext(template, url, height, width) {
   var iframe = [
-    '[[iframe ', url, ' style="width: ', width, '; height: ', height, '; border: 0; text-align: center;"]]',
+    '[[iframe ', url, ' style="width: ', width, '; height: ', height, '; border: 0; text-align: center;" scrolling="no"]]',
   ].join('');
 
   return template
