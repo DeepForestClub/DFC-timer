100596899e4f9e99fa4e4e27cbde10ba7136d6df
lest-day <106459514+lest-day@users.noreply.github.com>
2023-07-26 07:10:45 +0800

    Update timer-selector.js
    Signed-off-by: lest-day <106459514+lest-day@users.noreply.github.com>

--- timer-selector.js
+++ timer-selector.js
@@ -175,7 +175,7 @@ function buildUrl(language, startDate, durationMs, progressMessage, finishedMess
     parameters.append('style', styling);
   }
 
-  return 'https://deepforest-club.github.io/DFC-timer/timer.html?' + parameters;
+  return 'https://deepforestclub.github.io/DFC-timer/timer.html?' + parameters;
 }
 
 function buildWikitext(template, url, height, width) {
